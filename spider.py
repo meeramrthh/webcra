cebc040234aebb56ff794a003eefd8a696f9feb7
meeramrthh <meeramrthh@gmail.com>
2024-12-27 11:49:05 +0530

    8th commit

--- spider.py
+++ spider.py
@@ -14,5 +14,20 @@ class Spider:
     queue = set()       #waiting list (ram)
     crawled = set()     #crawled list
 
-    def __init__(self):
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + '/queue.txt'
+        Spider.crawled_file = Spider.project_name + '/crawled.txt'
+        self.boot()
+        self.crawl_page('First spider', Spider.base_url)
+
+    @staticmethod 
+    def boot():
+        create_project_dir(Spider.project_name)
+        create_data_files(Spider.project_name, Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
         
+    
