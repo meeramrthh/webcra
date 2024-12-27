3577ef2da8f74ace6374d8fd93757c175830510b
meeramrthh <meeramrthh@gmail.com>
2024-12-27 11:36:54 +0530

    7th commit

--- spider.py
+++ spider.py
@@ -1,4 +1,18 @@
+from urllib.request import urlopen #connect webpages from py
+from link_finder import LinkFinder
+from general import *
 
 
 class Spider:
-    
+
+    # Class Variables (shared among all instances/spiders)
+    project_name = ''
+    base_url = ''
+    domain_name = ''
+    queue_file = ''         #text file to save (harddrive)
+    crawled_file = ''
+    queue = set()       #waiting list (ram)
+    crawled = set()     #crawled list
+
+    def __init__(self):
+        
