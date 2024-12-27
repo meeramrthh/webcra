b6dc8ef7bc7a8fd6fc47652098da84ad86bfebac
meeramrthh <meeramrthh@gmail.com>
2024-12-27 13:16:01 +0530

    15th commit

--- domain.py
+++ domain.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlparse
 
-# get domain name (example.com)
+
+# Get domain name (example.com)
 def get_domain_name(url):
     try:
         results = get_sub_domain_name(url).split('.')
@@ -9,10 +10,9 @@ def get_domain_name(url):
         return ''
 
 
-# get sub domain name (name.example.com)
+# Get sub domain name (name.example.com)
 def get_sub_domain_name(url):
     try:
         return urlparse(url).netloc
     except:
         return ''
-    
--- general.py
+++ general.py
@@ -1,43 +1,41 @@
 import os
-# each website you crawl is a separate project (folder)
+
+
+# Each website is a separate project (folder)
 def create_project_dir(directory):
     if not os.path.exists(directory):
-        print('Creating project '+directory)
+        print('Creating directory ' + directory)
         os.makedirs(directory)
+
 
 # Create queue and crawled files (if not created)
 def create_data_files(project_name, base_url):
-    queue = project_name + '/queue.txt'
-    crawled = project_name + '/crawled.txt'
+    queue = os.path.join(project_name , 'queue.txt')
+    crawled = os.path.join(project_name,"crawled.txt")
     if not os.path.isfile(queue):
         write_file(queue, base_url)
     if not os.path.isfile(crawled):
         write_file(crawled, '')
 
 
-# create a new file
+# Create a new file
 def write_file(path, data):
-    f = open(path, 'w')
-    f.write(data)
-    f.close()
+    with open(path, 'w') as f:
+        f.write(data)
 
-# add data onto an existing file, stick a new link
+
+# Add data onto an existing file
 def append_to_file(path, data):
     with open(path, 'a') as file:
         file.write(data + '\n')
 
-#delete the contents of a file
+
+# Delete the contents of a file
 def delete_file_contents(path):
-    with open(path, 'w'):
-        pass #do nothing
+    open(path, 'w').close()
 
-#this is gonna be slow writing to the files, so now using variables
-#getting thebest of both worlds
 
-# links in file to set
-# items in set and save to file
-
-#read a file and convert each line to set items
+# Read a file and convert each line to set items
 def file_to_set(file_name):
     results = set()
     with open(file_name, 'rt') as f:
@@ -45,9 +43,9 @@ def file_to_set(file_name):
             results.add(line.replace('\n', ''))
     return results
 
-# iterate through a set, each item in the set will be a new line file 
-def set_to_file(links, file):
-    delete_file_contents(file)
-    for link in sorted(links):
-        append_to_file(file, link)
 
+# Iterate through a set, each item will be a line in a file
+def set_to_file(links, file_name):
+    with open(file_name,"w") as f:
+        for l in sorted(links):
+            f.write(l+"\n")
