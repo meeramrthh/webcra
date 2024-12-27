e5b650a65bcf63e07e1e21acacd4c7a628270668
meeramrthh <meeramrthh@gmail.com>
2024-12-27 10:56:11 +0530

    first commit

---
+++ general.py
@@ -0,0 +1,32 @@
+import os
+# each website you crawl is a separate project (folder)
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print('Creating project '+directory)
+        os.makedirs(directory)
+
+# Create queue and crawled files (if not created)
+def create_data_files(project_name, base_url):
+    queue = project_name + '/queue.txt'
+    crawled = project_name + '/crawled.txt'
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+
+# create a new file
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
+
+# add data onto an existing file, stick a new link
+def append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data + '\n')
+
+#delete the contents of a file
+def delete_file_contents(path):
+    with open(path, 'w'):
+        pass #do nothing
